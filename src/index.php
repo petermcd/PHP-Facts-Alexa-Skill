782b3b68717c7ae1d40d9212b05fd3c92d17976d
Peter <tutorials@alexabible.com>
2018-04-23 00:31:09 +0100

    disabled debug mode

--- src/index.php
+++ src/index.php
@@ -4,7 +4,6 @@ ini_set('display_errors', true);
 include 'handlers/standardHandler.php';
 include '../vendor/autoload.php';
 $alexa = new \alexa\alexa('amzn1.ask.skill.0fbaa5b3-a690-4493-b689-33dafa1126e7');
-$alexa->debug = true;
 $handler = new standardHandler();
 $alexa->registerHandler($handler, 'default');
 $alexa->run();
