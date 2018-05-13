b5183fee459076164fe70465a10ebe5060b17c97
Peter <tutorials@alexabible.com>
2018-05-13 16:16:08 +0100

    modified so that sqlite is in logging folder

--- src/index.php
+++ src/index.php
@@ -3,8 +3,11 @@ include 'handlers/standardHandler.php';
 include '../vendor/autoload.php';
 
 const APP_ID = '';
+$loggingPath = getcwd() . '/logging/log.sqlite';
 
 $alexa = new \alexa\alexa(APP_ID);
+$sqlite = new \alexa\logger\sqlite($loggingPath);
+$alexa->logger->addLogger($sqlite);
 $handler = new standardHandler();
 $alexa->registerHandler($handler, 'default');
 $alexa->run();
